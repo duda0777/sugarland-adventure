03078fa83f8d743edcf17670ada772bd26b35a49
duda0777 <dudamariahmn12337@gmail.com>
2025-10-23 23:39:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const startBtn = document.querySelector(".start-btn");
 
-  if (startBtn) {
-    startBtn.addEventListener("click", function () {
-      alert("🍬 Bem-vinda à aventura em Sugarland! Prepare-se para se divertir!");
-    });
-  }
-});
