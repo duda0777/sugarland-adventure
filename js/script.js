285c64c8c108ae6ead0e6826f1341060ddd59022
duda0777 <dudamariahmn12337@gmail.com>
2025-10-24 01:13:14 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-// Funções para os botões do menu
-document.querySelector('.menu').addEventListener('click', () => {
+document.querySelector('.start-area').addEventListener('click', () => {
+  alert('Fase 1 iniciada! 🍬');
+});
+
+document.querySelector('.menu-area').addEventListener('click', () => {
   alert('Você clicou em MENU!');
 });
 
-document.querySelector('.tutorial').addEventListener('click', () => {
-  alert('Bem-vindo ao tutorial! Aprenda a jogar Sugarland Adventure.');
-});
-
-document.querySelector('.start').addEventListener('click', () => {
-  alert('Fase 1 iniciada! Boa sorte 🍬');
+document.querySelector('.tutorial-area').addEventListener('click', () => {
+  alert('Bem-vindo ao tutorial!');
 });
