333e4b14148cd2dd51d6252ec95c778eda240f35
duda0777 <dudamariahmn12337@gmail.com>
2025-10-24 01:30:14 -0300

    Add event listener for level selection and start phase

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-document.querySelector('.start-area').addEventListener('click', () => {
-  alert('Fase 1 iniciada! 🍬');
-});
-
 document.querySelector('.menu-area').addEventListener('click', () => {
   alert('Você clicou em MENU!');
 });
 
 document.querySelector('.tutorial-area').addEventListener('click', () => {
   alert('Bem-vindo ao tutorial!');
+});
+
+document.querySelector('.nivel-area').addEventListener('click', () => {
+  alert('Escolha seu nível!');
+});
+
+document.querySelector('.start-area').addEventListener('click', () => {
+  alert('Fase 1 iniciada! 🍬');
 });
